3a1dbb4d2f1b98d8c80fe4f3d6b88d8a17dd8a42
JB Orozco <35300800+jborozco@users.noreply.github.com>
2023-05-16 18:39:56 +0200

    Assign userChoice value with buttons

--- script.js
+++ script.js
@@ -1,23 +1,57 @@
-// Ask the user to input one of them, make it non case sensitive
-//let userChoice = prompt('Please choose between "Rock", "Paper","Scissors"', "").toLowerCase();
+// Define User choice based on the chosen button
+
+let userChoice;
+
+let buttonPaper = document.querySelector('#buttonPaper');
+let buttonScissors = document.querySelector('#buttonScissors');
+let buttonRock = document.querySelector('#buttonRock');
 
 
-let checkUserChoice;
+buttonPaper.onclick = () => {
+    userChoice = "paper";
+    console.log(userChoice);
+    // Ask if the player wants to play again
 
+    let result = window.confirm(rockPaperScissors(userChoice) + `
+        
+Do you want to play again ? `);
 
-
-// Verify that the value entered by the user is correct 
-
-function checkChoice(choice) {
-
-    if (choice === "rock" || choice === "paper" || choice === "scissors") {
-        checkUserChoice = true;
-    } else {
-        checkUserChoice = false;
+    if (result === true) {
+        location.reload()
     }
-
-    return checkUserChoice;
 }
+
+buttonScissors.onclick = () => {
+    userChoice = "scissors";
+    console.log(userChoice);
+    // Ask if the player wants to play again
+
+    let result = window.confirm(rockPaperScissors(userChoice) + `
+        
+Do you want to play again ? `);
+
+    if (result === true) {
+        location.reload()
+    }
+}
+
+buttonRock.onclick = () => {
+    userChoice = "rock";
+    console.log(userChoice);
+    // Ask if the player wants to play again
+
+    let result = window.confirm(rockPaperScissors(userChoice) + `
+        
+Do you want to play again ? `);
+
+    if (result === true) {
+        location.reload()
+    }
+}
+
+
+
+
 
 // Assign a random choice to the computer 
 function computerChoice() {
@@ -35,47 +69,33 @@ function computerChoice() {
 }
 
 function rockPaperScissors(choice) {
-    checkChoice(choice);
 
     //If a correct value is chosen, make the computer choose one of the constant randomly
-    if (checkUserChoice) {
-        //Compare User & computer value + Show the appropriate message
 
-        let cpChoice = computerChoice();
-        if (cpChoice === userChoice) {
-            return (`Computer: ${computerChoice}
+    //Compare User & computer value + Show the appropriate message
+
+    let cpChoice = computerChoice();
+    if (cpChoice === userChoice) {
+        return (`Computer: ${computerChoice}
 Player: ${userChoice}
 
 EVEN GAME !`);
-        } else if (userChoice === "scissors" && cpChoice === "paper" ||
-            userChoice === "paper" && cpChoice === "rock" ||
-            userChoice === "rock" && cpChoice === "scissors") {
-            return (`Computer: ${cpChoice}
+    } else if (userChoice === "scissors" && cpChoice === "paper" ||
+        userChoice === "paper" && cpChoice === "rock" ||
+        userChoice === "rock" && cpChoice === "scissors") {
+        return (`Computer: ${cpChoice}
 Player: ${userChoice} 
                     
 YOU WON !`);
-        } else {
-            return (`Computer: ${cpChoice}
+    } else {
+        return (`Computer: ${cpChoice}
 Player: ${userChoice} 
 
 YOU LOST :'(`);
-        }
-
-
     }
-    //If the value is not right show an error message and ask the user to try again
-    else {
-        return ("Game off ! Please enter a correct value...");
 
-    }
+
+
 }
 
-// Ask if the player wants to play again
 
-let result = window.confirm(rockPaperScissors(userChoice) + `
-        
-Do you want to play again ? `);
-
-if (result === true) {
-    location.reload()
-}
